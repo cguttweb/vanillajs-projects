b1e337fb2a323b5239df038ff0274a2c723d39d2
Chloe <cguttweb@pm.me>
2022-01-11 18:47:41 +0000

    added localStorage to save data

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container')
-const seats = document.querySelectorAll('.row.seat:not(.occupied)')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
@@ -7,11 +7,21 @@ const movieSelect = document.getElementById('movie')
 // using + sets as typeof number could also wrap in parseInt()
 let ticketPrice = +movieSelect.value
 
+function saveMovieData(movieIndex, moviePrice){
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
 function updateSelectedCount(){
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
 
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+
+  // as array seatsIndex needs wrapping in JSON stringify
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
   const selectedSeatsCount = selectedSeats.length
-  console.log(selectedSeatsCount)
+
   count.innerText = selectedSeatsCount
   total.innerText = selectedSeatsCount * ticketPrice
 }
@@ -20,6 +30,7 @@ function updateSelectedCount(){
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value
   updateSelectedCount()
+  saveMovieData(e.target.selectedIndex, e.target.value)
 })
 
 // seat clicks
