41f599940977defce5d637bb7fe5d670abdc7206
Chloe <cguttweb@pm.me>
2022-01-11 18:58:21 +0000

    pulling data from localStorage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
+populateUI()
+
 // using + sets as typeof number could also wrap in parseInt()
 let ticketPrice = +movieSelect.value
 
@@ -26,6 +28,25 @@ function updateSelectedCount(){
   total.innerText = selectedSeatsCount * ticketPrice
 }
 
+function populateUI(){
+  // convert back to an array with JSON.parse
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+  
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+}
+
 // Movie select
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value
@@ -42,3 +63,4 @@ container.addEventListener('click', e => {
   }
 })
 
+updateSelectedCount()
